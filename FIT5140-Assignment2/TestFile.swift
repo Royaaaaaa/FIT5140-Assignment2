a6f604fb870143efc0d5ea06f9d36eadfe5b75b3
妈妈 <mac@dyn-118-139-36-227.its.monash.edu.au>
2018-09-16 14:04:55 +1000

    Test file

---
+++ FIT5140-Assignment2/TestFile.swift
@@ -0,0 +1,13 @@
+//
+//  TestFile.swift
+//  FIT5140-Assignment2
+//
+//  Created by 隋晓婷 on 2018/9/16.
+//  Copyright © 2018 Maya. All rights reserved.
+//
+
+import UIKit
+
+class TestFile: NSObject {
+
+}
